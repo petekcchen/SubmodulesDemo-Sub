b15a95ffcef932cf6e23da452ff5db83c132017a
Pete <pete.chen@afusion.com>
2014-05-11 00:34:15 +0800

    add project and solution

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubmodulesDemo_Sub
+{
+    public class Class1
+    {
+    }
+}
